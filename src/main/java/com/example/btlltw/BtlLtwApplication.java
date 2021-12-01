849b793ffe7a80ace8609b97a0bc7e8b4ba3280a
ryan2kptit <nguyenquangdong28092000@gmail.com>
2021-12-01 15:04:18 +0700

    update

--- src/main/java/com/example/btlltw/BtlLtwApplication.java
+++ src/main/java/com/example/btlltw/BtlLtwApplication.java
@@ -3,6 +3,7 @@ package com.example.btlltw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
