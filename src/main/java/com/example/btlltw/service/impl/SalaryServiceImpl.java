2a36212135444dbcb691a4edb6551d8630e18201
ryan2kptit <nguyenquangdong28092000@gmail.com>
2021-12-11 06:59:20 +0700

    update code

--- src/main/java/com/example/btlltw/service/impl/SalaryServiceImpl.java
+++ src/main/java/com/example/btlltw/service/impl/SalaryServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -37,82 +38,88 @@ public class SalaryServiceImpl implements SalaryService {
         List<Integer> listSumComplexityMainDriver = salaryRepository.getListSumComplexityMainDriver();
         List<RouteAndMainDriverDto> listMain = new ArrayList<>();
         int i = 0;
-        for(Driver element: listMainDriver){
+        for (Driver element : listMainDriver) {
             RouteAndMainDriverDto routeAndMainDriverDto = new RouteAndMainDriverDto();
             routeAndMainDriverDto.setDriver(element);
             routeAndMainDriverDto.setComplexity(listSumComplexityMainDriver.get(i++));
             listMain.add(routeAndMainDriverDto);
         }
         Salary mainDriverSalary = salaryRepository.getById(1);
-        for(RouteAndMainDriverDto element: listMain){
+        for (RouteAndMainDriverDto element : listMain) {
             element.setComplexity((int) element.getComplexity() * mainDriverSalary.getIndexMoney());
         }
         List<Driver> listExtraDriver = driverService.getExtraDriver();
         List<Integer> listSumComplexityExtraDriver = salaryRepository.getListSumComplexityExtraDriver();
         List<RouteAndMainDriverDto> listExtra = new ArrayList<>();
         int j = 0;
-        for(Driver element: listExtraDriver){
+        for (Driver element : listExtraDriver) {
             RouteAndMainDriverDto routeAndExtraDriverDto = new RouteAndMainDriverDto();
             routeAndExtraDriverDto.setDriver(element);
             routeAndExtraDriverDto.setComplexity(listSumComplexityExtraDriver.get(j++));
             listExtra.add(routeAndExtraDriverDto);
         }
         Salary extraDriverSalary = salaryRepository.getById(2);
-        for(RouteAndMainDriverDto element: listExtra){
+        for (RouteAndMainDriverDto element : listExtra) {
             element.setComplexity((int) element.getComplexity() * extraDriverSalary.getIndexMoney());
         }
         List<SalaryStatisticDto> salaryStatisticDtoList = new ArrayList<>();
-        for(RouteAndMainDriverDto element: listMain){
+        for (RouteAndMainDriverDto element : listMain) {
             SalaryStatisticDto salaryStatisticDto = new SalaryStatisticDto();
-            int k = 0;
-            System.out.println(element + "main");
-            for(RouteAndMainDriverDto item: listExtra) {
-                System.out.println(item + "extra");
-                if(element.getDriver().getId() == item.getDriver().getId()){
+            for (RouteAndMainDriverDto item : listExtra) {
+                if (element.getDriver().getId() == item.getDriver().getId()) {
                     salaryStatisticDto.setSalary(element.getComplexity() + item.getComplexity());
                     salaryStatisticDto.setDriver(element.getDriver());
                     salaryStatisticDtoList.add(salaryStatisticDto);
-                    listExtra.remove(item);
-                    k = 1;
-                    if(listExtra.size() <= 1) break;
+                    break;
                 }
             }
-            if(k==0) {
-                System.out.println(element + "main 2");
-                salaryStatisticDto.setDriver(element.getDriver());
-                salaryStatisticDto.setSalary(element.getComplexity());
-                salaryStatisticDtoList.add(salaryStatisticDto);
-            }
         }
-        if(listExtra.size() > 0) {
-            for(RouteAndMainDriverDto item: listExtra) {
-                SalaryStatisticDto salaryStatisticDto = new SalaryStatisticDto();
-                salaryStatisticDto.setDriver(item.getDriver());
-                salaryStatisticDto.setSalary(item.getComplexity());
-                salaryStatisticDtoList.add(salaryStatisticDto);
+            System.out.println(listMain.size());
+            List<SalaryStatisticDto> newSalaryStatistic = new ArrayList<>();
+            for (RouteAndMainDriverDto element : listMain) {
+                for (SalaryStatisticDto item : salaryStatisticDtoList) {
+                    if (element.getDriver().getId() != item.getDriver().getId()) {
+                        SalaryStatisticDto salaryStatisticDto = new SalaryStatisticDto();
+                        salaryStatisticDto.setSalary(element.getComplexity());
+                        salaryStatisticDto.setDriver(element.getDriver());
+                        newSalaryStatistic.add(salaryStatisticDto);
+
+                    }
+                }
             }
+
+            for (RouteAndMainDriverDto element : listExtra) {
+                for (SalaryStatisticDto item : salaryStatisticDtoList) {
+                    if (element.getDriver().getId() != item.getDriver().getId()) {
+                        SalaryStatisticDto salaryStatisticDto = new SalaryStatisticDto();
+                        salaryStatisticDto.setSalary(element.getComplexity());
+                        salaryStatisticDto.setDriver(element.getDriver());
+                        newSalaryStatistic.add(salaryStatisticDto);
+                    }
+                }
+            }
+            System.out.println(salaryStatisticDtoList);
+            salaryStatisticDtoList.addAll(newSalaryStatistic);
+            return salaryStatisticDtoList;
         }
-        System.out.println(salaryStatisticDtoList);
-        return salaryStatisticDtoList;
-    }
 
-    public Salary updateSalaryById(int id, Salary salary) {
-        Salary exitSalary = salaryRepository.getById(id);
-        if(exitSalary == null) throw new NotFoundException();
-        exitSalary.setName(salary.getName());
-        exitSalary.setIndexMoney(salary.getIndexMoney());
-        Salary updateSalary = salaryRepository.save(exitSalary);
-        return updateSalary;
-    }
+        public Salary updateSalaryById ( int id, Salary salary){
+            Salary exitSalary = salaryRepository.getById(id);
+            if (exitSalary == null) throw new NotFoundException();
+            exitSalary.setName(salary.getName());
+            exitSalary.setIndexMoney(salary.getIndexMoney());
+            Salary updateSalary = salaryRepository.save(exitSalary);
+            return updateSalary;
+        }
 
-    public Boolean deleteSalaryById(int id) {
-        Salary exitSalary = salaryRepository.getById(id);
-        if(exitSalary == null) throw new NotFoundException();
-        salaryRepository.deleteById(id);
-        return true;
-    }
+        public Boolean deleteSalaryById ( int id){
+            Salary exitSalary = salaryRepository.getById(id);
+            if (exitSalary == null) throw new NotFoundException();
+            salaryRepository.deleteById(id);
+            return true;
+        }
 
-    public  List<Salary> getAllSalary() {
-        return salaryRepository.findAll();
-    }
+        public List<Salary> getAllSalary () {
+            return salaryRepository.findAll();
+        }
 }
